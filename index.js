6481710ed0569e9fa8cecf4dc17e1ce87c178287
Benevanio dos Santos Conceicao <benevanio.conceicao@sysmap.com.br>
2024-12-11 13:37:28 -0300

    add bodyparser

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 const express = require('express');
 const app = express();
+const bodyParser = require('body-parser');
 app.set('view engine', 'ejs');
-
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static('public'));
 
 app.get('/', (req, res) => {
